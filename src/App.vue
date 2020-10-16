5b34885dd7b97838d502bdafe2fb46c3e8ca2589
Hanna Edlund <hanna@edlunds.biz>
2020-10-16 13:08:36 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,111 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar
+        app
+        color="white"
+        flat
+    >
+      <h1 class="brown--text text--lighten-2">Sveduddens kennel</h1>
+      <v-container>
+        <v-avatar
+            color="#dcc7ac"
+            size="32"
+        >
+          <v-icon>mdi-dog</v-icon>
+        </v-avatar>
+
+        <v-tabs fixed-tabs color="#b58950" class="ml-10">
+          <v-tab
+              v-for="link in links"
+              :key="link"
+              text>
+            {{ link }}
+          </v-tab>
+        </v-tabs>
+
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="background">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item
+                    v-for="n in 5"
+                    :key="n"
+                    link
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      <v-avatar
+                          class="mr-10"
+                          color="#dcc7ac"
+                          size="32"
+                      >
+                        <v-icon>mdi-dog</v-icon>
+                      </v-avatar>
+                      {{ n }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                    link
+                    color="grey lighten-4"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      Refresh
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+                min-height="70vh"
+                rounded="lg"
+            >
+                <v-card-text>
+                  <h1 class="my-2">Välkommen!</h1>
+                  <p>FCI och SKK-registrerad uppfödare av Jack Russell Terrier sen 2001. Sveduddens Kennel finns 6 mil nordost om Stockholm.<br><br>
+
+                    Över 50 HP belönade uppfödargrupper!</p>
+                </v-card-text>
+
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    links: [
+      'Start',
+      'News',
+      'Sveduddens Kennel',
+      'Hundarna',
+      'Valpar',
+      'Champions',
+      'Galleri',
+      'Kontakt',
+      'Länkar'
+    ],
+  }),
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.background{
+  background-color: #ebdfd0;
 }
 </style>
